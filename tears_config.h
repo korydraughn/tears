fd39e0dac58ba0187db3f7bf444a9d21843e6edc
Kory Draughn <korydraughn@renci.org>
2019-08-13 10:15:49 -0400

    Worked like a charm baby!

---
+++ tears_config.h
@@ -0,0 +1,12 @@
+#ifndef TEARS_CONFIG_H
+#define TEARS_CONFIG_H
+
+#define PACKAGE             "tears"
+#define PACKAGE_STRING      "1.0.0"
+#define PACKAGE_BUGREPORT   "whitwham@github"
+#define PACKAGE_URL         "https://github.com/korydraughn/tears"
+#define PACKAGE_NAME        "tears"
+#define PACKAGE_VERSION     "1.0.0"
+
+#endif // TEARS_CONFIG_H
+
